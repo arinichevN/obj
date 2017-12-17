df3f6bd05ecfcba83b6462be09e2ea337263235c
Arinichev Nikolay <arinichev_n@mail.ru>
2017-12-17 18:46:12 +0300

    c

--- sql/data.sql
+++ sql/data.sql
@@ -1,13 +1,13 @@
 CREATE TABLE "prog"
 (
-  "id" INTEGER PRIMARY KEY,
-  "heater_id" INTEGER NOT NULL,--heater_id and cooler_id shall be unique
-  "cooler_id" INTEGER NOT NULL,--heater_id and cooler_id shall be unique
-  "ambient_temperature" REAL NOT NULL,
+  "id" INTEGER PRIMARY KEY, -- (regulater uses it)
+  "heater_id" INTEGER NOT NULL,--heater_id and cooler_id shall be unique (regulater uses it)
+  "cooler_id" INTEGER NOT NULL,--heater_id and cooler_id shall be unique (regulater uses it)
+  "ambient_temperature" REAL NOT NULL,--matter temperature will aim to it
   "matter_mass" REAL NOT NULL,
   "matter_ksh" REAL NOT NULL,--specific heat
   "loss_factor" REAL NOT NULL,-- >= 0 
-  "temperature_pipe_length" INTEGER NOT NULL,
+  "temperature_pipe_length" INTEGER NOT NULL,--delay between getting energy and increasing temperature
 
   "enable" INTEGER NOT NULL,
   "load" INTEGER NOT NULL
